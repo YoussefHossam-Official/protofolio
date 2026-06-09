a5f48072acfe89dcaf31cb0e24ed7d5ac7fe5ccf
YoussefHossam-Official <youssef@example.com>
2026-06-09 12:28:31 +0300

    fix: export Express app directly, no serverless-http

--- api/index.js
+++ api/index.js
@@ -1,7 +1,2 @@
-// da el entry point beta3 Vercel
-// hna Express byt7wal le serverless function 3ashan y4ta8al 3ala Vercel
-const serverlessHttp = require('serverless-http');
 const app = require('../back/src/app');
-
-// bn export el app wrapped bel serverless-http
-module.exports = serverlessHttp(app);
+module.exports = app;
